1d70ca8c15cb332a5b8de4969e5b1c9d4f339835
jdgwartney <davidg@boundary.com>
2015-09-16 17:28:44 -0700

    Starting test case

---
+++ src/test/java/com/bmc/pulse/api/examples/MeasurementToJSONTest.java
@@ -0,0 +1,32 @@
+package com.bmc.pulse.api.examples;
+
+import org.junit.*;
+
+import static org.junit.Assert.*;
+/**
+ * Created by davidg on 9/16/15.
+ */
+public class MeasurementToJSONTest {
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void testJSON() throws InterruptedException {
+        Assert.assertTrue(true);
+    }
+
+}
